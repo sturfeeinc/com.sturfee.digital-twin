566f885ad091d3d8480bf4d93a6d07e4370cf983
kush <kushagrag@sturfee.com>
2022-06-17 11:16:24 -0700

    package name update

--- Runtime/Config/DTConstants.cs
+++ Runtime/Config/DTConstants.cs
@@ -9,14 +9,7 @@ namespace Sturfee.DigitalTwin
         public static readonly string LOCAL_SPACES_PATH = "DigitalTwin/Spaces";
         public static readonly string LOCAL_ASSETS_PATH = "DigitalTwin/Assets";
 
-#if DTE_DEV
-        public static readonly string SPACES_API = "https://digitwin.devsturfee.com/api/v2.0";
-        public static readonly string DTE_API = "https://digitwin.devsturfee.com/api/v2.0";
-
-#else
         public static readonly string SPACES_API = "https://sharedspaces-api.sturfee.com/api/v2.0";
         public static readonly string DTE_API = "https://sharedspaces-api.sturfee.com/api/v2.0";
-#endif
-
     }
 }
