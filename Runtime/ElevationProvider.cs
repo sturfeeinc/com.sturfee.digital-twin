c3635e68467db2cd687919d628a9d6bc52e316bb
kush <kushagrag@sturfee.com>
2022-09-30 14:59:05 -0700

    raycasting bug fixed on ElevationProvider, optional params added for layers and stepoffset

--- Runtime/ElevationProvider.cs
+++ Runtime/ElevationProvider.cs
@@ -9,26 +9,29 @@ namespace Sturfee.DigitalTwin
 {
     public class ElevationProvider : Singleton<ElevationProvider>
     {
-        public float GetTerrainElevation(Vector3 pos)
+        public float GetTerrainElevation(Vector3 pos, float stepOffset = 1, int layerMask = -1)
         {
             RaycastHit hit;
             Ray ray;
             float elevation = 0.0f;
-
+            if(layerMask == -1)
+            {
+                layerMask = LayerMask.GetMask(SturfeeLayers.Terrain, $"{XrLayers.Terrain}");
+            }
 
             // 1st raycast to get terrain elevation
             pos.y += 1000;       // TODO: what if terrain is higher than 1000m ?
             ray = new Ray(pos, Vector3.down);
-            Debug.DrawRay(ray.origin, ray.direction, Color.yellow, 1.0f);
-            if (Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask(SturfeeLayers.Terrain, $"{XrLayers.Terrain}")))
+            //Debug.DrawRay(ray.origin, ray.direction * 10000, Color.yellow, 1.0f);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
             {
                 elevation = hit.point.y;
             }
 
             // 2nd raycast to start slightly above terrain ( similar to step offset of character controller)
-            pos.y = elevation + 1.499f ;     // TODO: Should be made configurable from inspector
+            pos.y = elevation + stepOffset ;     // TODO: Should be made configurable from inspector
             ray = new Ray(pos, Vector3.down);
-            Debug.DrawRay(ray.origin, ray.direction, Color.green, 1.0f);
+            //Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green, 1.0f);
             if (Physics.Raycast(ray, out hit))
             {
                 elevation = hit.point.y;
@@ -37,7 +40,7 @@ namespace Sturfee.DigitalTwin
             return elevation;
         }
 
-        public float GetTerrainElevation(double latitude, double longitude)
+        public float GetTerrainElevation(double latitude, double longitude, float stepOffset = 1, int layerMask = -1)
         {
             var location = new GeoLocation { Latitude = latitude, Longitude = longitude };
             var local = Converters.GeoToUnityPosition(location);
@@ -45,7 +48,7 @@ namespace Sturfee.DigitalTwin
             return GetTerrainElevation(local);
         }
 
-        public float GetTerrainElevation(GeoLocation location)
+        public float GetTerrainElevation(GeoLocation location, float stepOffset = 1, int layerMask = -1)
         {
             return GetTerrainElevation(location.Latitude, location.Longitude);
         }
