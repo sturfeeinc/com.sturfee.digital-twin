0939b1f0b6f47053c359c80bb6fe7025439b84fd
kush <kushagrag@sturfee.com>
2022-10-05 10:50:34 -0700

    some debug code added

--- Runtime/ElevationProvider.cs
+++ Runtime/ElevationProvider.cs
@@ -13,28 +13,31 @@ namespace Sturfee.DigitalTwin
         {
             RaycastHit hit;
             Ray ray;
-            float elevation = 0.0f;
+            float elevation = -1000.0f;
             if(layerMask == -1)
             {
+                Debug.Log(" layermask is -1");
                 layerMask = LayerMask.GetMask(SturfeeLayers.Terrain, $"{XrLayers.Terrain}");
-            }
+            }            
 
             // 1st raycast to get terrain elevation
             pos.y += 1000;       // TODO: what if terrain is higher than 1000m ?
             ray = new Ray(pos, Vector3.down);
-            //Debug.DrawRay(ray.origin, ray.direction * 10000, Color.yellow, 1.0f);
+            Debug.DrawRay(ray.origin, ray.direction * 10000, Color.yellow, 1.0f);
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
             {
                 elevation = hit.point.y;
+                //Debug.Log($" Elevation 1 : {elevation} name : {hit.transform.name}");
             }
 
             // 2nd raycast to start slightly above terrain ( similar to step offset of character controller)
             pos.y = elevation + stepOffset ;     // TODO: Should be made configurable from inspector
             ray = new Ray(pos, Vector3.down);
-            //Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green, 1.0f);
+            Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green, 1.0f);
             if (Physics.Raycast(ray, out hit))
             {
                 elevation = hit.point.y;
+                //Debug.Log($" Elevation 2 : {elevation} name : {hit.transform.name}");
             }
 
             return elevation;
