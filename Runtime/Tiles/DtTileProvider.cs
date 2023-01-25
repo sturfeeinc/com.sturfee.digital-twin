bcceb631b0ae8caac40ec36876792774a50f4a54
JayO <119442754+JayOSturfee@users.noreply.github.com>
2023-01-25 15:19:54 -0600

    DT Outdoor Services revision
    DT Outdoor Services revision

--- Runtime/Tiles/DtTileProvider.cs
+++ Runtime/Tiles/DtTileProvider.cs
@@ -3,6 +3,7 @@ using SturfeeVPS.Core;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 using System.Threading.Tasks;
 using UnityEngine.Networking;
@@ -11,11 +12,9 @@ namespace Sturfee.DigitalTwin.Tiles
 {
     public interface ITileProvider
     {
-        Task<List<DigitalTwinTileItem>> FetchTileUrl(string geohash);
-        Task<List<DigitalTwinTileItem>> FetchTileUrls(List<string> geohashes);
-        Task<string> DownloadTileLayer(string geohash, string url, string filename);
+        Task<DigitalTwinTileItem> FetchTileUrl(string geohash);
+        Task<string> DownloadTileLayers(string geohash, string url);
     }
-
 
     public class DtTileProvider : ITileProvider
     {
@@ -24,33 +23,72 @@ namespace Sturfee.DigitalTwin.Tiles
             ServicePointManager.DefaultConnectionLimit = 1000; 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
-
-        public async Task<string> DownloadTileLayer(string geohash, string layer, string url)
+        public async Task<DigitalTwinTileItem> FetchTileUrl(string geohash)
         {
-            MyLogger.Log($"DtTileProvider :: Downloading Tile {geohash} => {layer} | {url} ");
-            var dtTileCache = IOC.Resolve<ICacheProvider<CachedDtTile>>();
-
-            var tileFolder = Path.Combine(dtTileCache.CacheDir, $"{geohash}");
-            if (!Directory.Exists(tileFolder)) { Directory.CreateDirectory(tileFolder); }
-            var newTilePath = Path.Combine(tileFolder, $"{layer}");
+            // get download URL
+            string url = DtConstants.DTE_OUTDOOR_TILES_API + "/" + geohash;
 
             try
             {               
                 var uwr = new UnityWebRequest(url);
+
+                var dh = new DownloadHandlerBuffer();
+                uwr.downloadHandler = dh;
+
                 uwr.method = UnityWebRequest.kHttpVerbGET;
-                var dh = new DownloadHandlerFile($"{newTilePath}");
+                await uwr.SendWebRequest();
+                
+                
+
+                if (uwr.result == UnityWebRequest.Result.ConnectionError) //uwr.isNetworkError || uwr.isHttpError)
+                {
+                }
+                else
+                {
+                    var item = JsonConvert.DeserializeObject<DigitalTwinTileItem>(uwr.downloadHandler.text);
+                    return item;
+                }
+
+            }
+            catch (Exception e)
+            {           
+            }
+
+            return null;
+        }
+
+        public async Task<string> DownloadTileLayers(string geohash, string url)
+        {
+            MyLogger.Log($"DtTileProvider :: Downloading Tile Layers for {geohash}");
+            var dtTileCache = IOC.Resolve<ICacheProvider<CachedDtTile>>();
+
+            var tileFolder = Path.Combine(dtTileCache.CacheDir, $"{geohash}");
+            var tileZip = Path.Combine(tileFolder, $"{geohash}.zip");
+            if (!Directory.Exists(tileFolder)) { Directory.CreateDirectory(tileFolder); }
+
+            try
+            {               
+                var uwr = new UnityWebRequest(url);
+
+                uwr.method = UnityWebRequest.kHttpVerbGET;
+                var dh = new DownloadHandlerFile($"{tileZip}");
                 dh.removeFileOnAbort = true;
                 uwr.downloadHandler = dh;
                 await uwr.SendWebRequest();
 
-                if (uwr.result == UnityWebRequest.Result.ConnectionError) //uwr.isNetworkError || uwr.isHttpError)
+                if (uwr.result != UnityWebRequest.Result.Success) //(uwr.result == UnityWebRequest.Result.ConnectionError) //uwr.isNetworkError || uwr.isHttpError)
                 {
                     MyLogger.LogError(uwr.error);
-                    MyLogger.LogError($"DtTileProvider :: ERROR Downloading Tile {geohash} => {layer} | {url} \n{uwr.error}");
+                    MyLogger.LogError($"DtTileProvider :: ERROR Downloading Tile {geohash}");
                 }
                 else
                 {
-                    MyLogger.Log("DtTileProvider :: Download saved to: " + newTilePath.Replace("/", "\\") + "\r\n" + uwr.error);
+                    MyLogger.Log("DtTileProvider :: Download saved to: " + tileZip.Replace("/", "\\") + "\r\n" + uwr.error);
+
+                    // extract zip file
+                    ZipFile.ExtractToDirectory(tileZip, tileFolder);
+                    // delete zip file
+                    File.Delete(tileZip);
                 }
 
             }
@@ -60,47 +98,7 @@ namespace Sturfee.DigitalTwin.Tiles
                 throw e;                
             }
 
-            return newTilePath;
-        }
-
-        public Task<List<DigitalTwinTileItem>> FetchTileUrl(string geohash)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public async Task<List<DigitalTwinTileItem>> FetchTileUrls(List<string> geohashes)
-        {
-            MyLogger.Log($"Request = POST {DtConstants.DTE_API}/digitaltwin/tiles");
-
-            // get download URL
-            var keyPrepend = "char7geohash";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"{DtConstants.DTE_API}/digitaltwin/tiles?keyPrepend={keyPrepend}");
-            request.Method = "POST";
-            request.ContentType = "application/json; charset=utf-8";
-            //await AuthHelper.AddXrcsTokenAuthHeader(request);
-
-            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-            {
-                string json = JsonConvert.SerializeObject(geohashes);
-                MyLogger.Log($"   FetchTileUrls geohashes = {json}");
-
-                streamWriter.Write(json);
-                streamWriter.Flush();
-            }
-
-            var response = await request.GetResponseAsync() as HttpWebResponse;
-            if (response.StatusCode != HttpStatusCode.OK)
-            {
-                MyLogger.LogError($"ERROR:: API => {response.StatusCode} - {response.StatusDescription}");
-            }
-
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            string jsonResponse = reader.ReadToEnd();
-
-            MyLogger.Log($"Download Asset Response from API:\n{jsonResponse}");
-            var items = JsonConvert.DeserializeObject<List<DigitalTwinTileItem>>(jsonResponse);
-
-            return items;
+            return tileZip;
         }
     }
 }
