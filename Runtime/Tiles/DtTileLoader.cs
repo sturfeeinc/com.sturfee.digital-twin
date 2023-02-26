b0e8e5ad6657768edd07ebba93eaaf79d2b8bf71
brentm <brentmcferrin@gmail.com>
2023-02-26 05:21:46 -0500

    add tile loading based on radius input

--- Runtime/Tiles/DtTileLoader.cs
+++ Runtime/Tiles/DtTileLoader.cs
@@ -77,15 +77,20 @@ namespace Sturfee.DigitalTwin.Tiles
         private int _geohashLength = 7;
         private List<LoadedTile> _loadedTiles;
 
+        public DtTileLoader()
+        {
+
+        }
+
         /// <summary>
         /// Checks if all the tiles (including neighbors) of this geocache are already downloaded and saved in cache
         /// </summary>
         /// <param name="geoHash"></param>
         /// <returns></returns>
-        public bool AvailableInCache(string geoHash)
+        public bool AvailableInCache(string geoHash, double radius = 150)
         {
             var location = GeoHash.Decode(geoHash);
-            return AvailableInCache(location.Coordinates.Lat, location.Coordinates.Lon);
+            return AvailableInCache(location.Coordinates.Lat, location.Coordinates.Lon, radius);
         }
 
         /// <summary>
@@ -94,9 +99,9 @@ namespace Sturfee.DigitalTwin.Tiles
         /// <param name="latitude"></param>
         /// <param name="longitude"></param>
         /// <returns></returns>
-        public bool AvailableInCache(double latitude, double longitude)
+        public bool AvailableInCache(double latitude, double longitude, double radius = 150)
         {
-            var cacheInfo = GetCacheInfo(latitude, longitude);
+            var cacheInfo = GetCacheInfo(latitude, longitude, radius);
 
             if (cacheInfo.NonCachedGeohashes.Any())
             {
@@ -114,10 +119,10 @@ namespace Sturfee.DigitalTwin.Tiles
         /// <param name="progress"></param>
         /// <param name="onError"></param>
         /// <returns></returns>
-        public async Task<List<string>> DownloadTilesAt(string geoHash, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
+        public async Task<List<string>> DownloadTilesAt(string geoHash, double radius = 150, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
         {
             var location = GeoHash.Decode(geoHash);
-            return await DownloadTilesAt(location.Coordinates.Lat, location.Coordinates.Lon, progress, onError);
+            return await DownloadTilesAt(location.Coordinates.Lat, location.Coordinates.Lon, radius, progress, onError);
         }
 
         /// <summary>
@@ -129,6 +134,19 @@ namespace Sturfee.DigitalTwin.Tiles
         /// <param name="onError"></param>
         /// <returns></returns>
         public async Task<List<string>> DownloadTilesAt(double latitude, double longitude, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
+        {
+            return await DownloadTilesAt(latitude, longitude, 150, progress, onError);
+        }
+
+        /// <summary>
+        /// Download all the tiles needed for this locatio (3x3)
+        /// </summary>
+        /// <param name="latitude"></param>
+        /// <param name="longitude"></param>
+        /// <param name="progress"></param>
+        /// <param name="onError"></param>
+        /// <returns></returns>
+        public async Task<List<string>> DownloadTilesAt(double latitude, double longitude, double radius, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
         {
             MyLogger.Log($"DtTileLoader :: Downloading tiles at {latitude}, {longitude}");
 
@@ -184,12 +202,12 @@ namespace Sturfee.DigitalTwin.Tiles
         /// <param name="progress"></param>
         /// <param name="onError"></param>
         /// <returns></returns>
-        public async Task<GameObject> LoadTilesAt(double latitude, double longitude, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
+        public async Task<GameObject> LoadTilesAt(double latitude, double longitude, double radius = 150, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
         {
             MyLogger.Log($"DigitalTwinTileLoader :: Trying to load DT Tiles for loc={latitude},{longitude}");
 
             var tileGeohash = GeoHash.Encode(latitude, longitude, _geohashLength);
-            await LoadTilesAt(tileGeohash, progress, onError);
+            await LoadTilesAt(tileGeohash, radius, progress, onError);
 
             return _parent;
             //await Task.Delay(2000);
@@ -202,7 +220,7 @@ namespace Sturfee.DigitalTwin.Tiles
         /// <param name="progress"></param>
         /// <param name="onError"></param>
         /// <returns></returns>
-        public async Task<GameObject> LoadTilesAt(string tileGeohash, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
+        public async Task<GameObject> LoadTilesAt(string tileGeohash, double radius = 150, DtTileLoadEvent progress = null, DtTileLoadError onError = null)
         {         
             var location = GeoHash.Decode(tileGeohash);
 
@@ -210,7 +228,7 @@ namespace Sturfee.DigitalTwin.Tiles
             _parent.transform.position = Vector3.zero;
             _loadedTiles = new List<LoadedTile>();
 
-            var cacheInfo = GetCacheInfo(location.Coordinates.Lat, location.Coordinates.Lon);
+            var cacheInfo = GetCacheInfo(location.Coordinates.Lat, location.Coordinates.Lon, radius);
             var cachedTiles = cacheInfo.CachedTilesPath;
             var filePaths = new List<string>(cachedTiles);
 
@@ -219,7 +237,8 @@ namespace Sturfee.DigitalTwin.Tiles
             if (nonCachedGeohashes.Any())
             {
                 var downloadedFilePaths = await DownloadTilesAt(
-                    tileGeohash, 
+                    tileGeohash,
+                    radius,
                     (downloadProgress, error) => 
                     {
                         // Split total progess into 2 parts => downloadProgress, importProgress
@@ -277,7 +296,7 @@ namespace Sturfee.DigitalTwin.Tiles
 
             progress?.Invoke(1,errorCount);
 
-            MyLogger.Log($"DtTileLoader :: DONE! Loaded (3x3) DT Tiles for loc={location.Coordinates.Lat},{location.Coordinates.Lon}");
+            MyLogger.Log($"DtTileLoader :: DONE! Loaded ({_loadedTiles.Count}) DT Tiles for loc={location.Coordinates.Lat},{location.Coordinates.Lon}");
             return _parent;
         }
 
@@ -388,17 +407,11 @@ namespace Sturfee.DigitalTwin.Tiles
             }
         }
 
-        private CacheInfo GetCacheInfo(double latitude, double longitude)
+        private CacheInfo GetCacheInfo(double latitude, double longitude, double radius = 150)
         {
             MyLogger.Log($"DtTileLoader :: Getting cache info for tiles at {latitude}, {longitude}");
-
-            var geohashes = new List<string>();
-            var thisGeohash = GeoHash.Encode(latitude, longitude, _geohashLength);
-            MyLogger.Log($"DtTileLoader : this geoHash = {thisGeohash}");
-
-            geohashes.Add(thisGeohash);
-            geohashes.AddRange(GeoHash.Neighbors(thisGeohash)); // Find all 8 geohash neighbors [n, ne, e, se, s, sw, w, nw] of a geohash string
-            MyLogger.Log($"DtTileLoader :: Looking for geohashes = {JsonConvert.SerializeObject(geohashes)} in cache");
+            var geohashes = GeoHashUtils.EncodeWithRadius(latitude, longitude, radius, _geohashLength);
+            MyLogger.Log($"DtTileLoader :: Looking for ({geohashes.Length}) geohashes = {JsonConvert.SerializeObject(geohashes)} in cache");
 
             var nonCachedGeohashes = new List<string>(geohashes);
 
@@ -537,6 +550,110 @@ namespace Sturfee.DigitalTwin.Tiles
             mesh.normals = normals;
 
             dupe.GetComponent<MeshCollider>().sharedMesh = mesh;
+        }
+    }
+}
+
+namespace SturfeeVPS.SDK
+{
+    public class GeoHashUtils
+    {
+        private static readonly double[] GRID_WIDTHS = new double[] { 5009400.0, 1252300.0, 156500.0, 39100.0, 4900.0, 1200.0, 152.9, 38.2, 4.8, 1.2, 0.149, 0.0370 };
+        private static readonly double[] GRID_HEIGHTS = new double[] { 4992600.0, 624100.0, 156000.0, 19500.0, 4900.0, 609.4, 152.4, 19.0, 4.8, 0.595, 0.149, 0.0199 };
+
+        private class Vector2Doubles
+        {
+            public double x;
+            public double y;
+            public Vector2Doubles(double _x, double _y)
+            {
+                x = _x;
+                y = _y;
+            }
+        }
+
+        public static string[] EncodeWithRadius(double latitude, double longitude, double radius, int precision, int minlevel = 1, int maxlevel = 12)
+        {
+            return CreateGeohashList(latitude, longitude, radius, precision, minlevel, maxlevel);
+        }
+
+        private static bool IsInsideOfCircle(double latitude, double longitude, double centre_lat, double centre_lon, double radius)
+        {
+            double x_diff = longitude - centre_lon;
+            double y_diff = latitude - centre_lat;
+
+            if (Math.Pow(x_diff, 2) + Math.Pow(y_diff, 2) <= Math.Pow(radius, 2)) return true;
+            return false;
+
+        }
+
+        private static Vector2Doubles GetCentroid(double latitude, double longitude, double height, double width)
+        {
+            double y_cen = latitude + (height / 2);
+            double x_cen = longitude + (width / 2);
+            return new Vector2Doubles(x_cen, y_cen);
+        }
+
+        private static Vector2Doubles PointToLatLon(double y, double x, double latitude, double longitude)
+        {
+            double pi = 3.14159265359;
+            double r_earth = 6371000;
+
+            double lat_diff = (y / r_earth) * (180 / pi);
+            double lon_diff = (x / r_earth) * (180 / pi) / Math.Cos(latitude * pi / 180);
+
+            double final_lat = latitude + lat_diff;
+            double final_lon = longitude + lon_diff;
+
+            return new Vector2Doubles(final_lat, final_lon);
+        }
+
+        private static string[] CreateGeohashList(double latitude, double longitude, double radius, int precision, int minlevel, int maxlevel)
+        {
+            if (radius == 0)
+            {
+                return new string[] { GeoHash.Encode(latitude, longitude, precision) };
+            }
+
+            double x = 0.0;
+            double y = 0.0;
+
+            List<Vector2Doubles> points = new List<Vector2Doubles>();
+
+            double height = (GRID_HEIGHTS[precision - 1]) / 2;
+            double width = (GRID_WIDTHS[precision - 1]) / 2;
+
+            int lat_moves = (int)(Math.Ceiling(radius / height)); // 4
+            int lon_moves = (int)(Math.Ceiling(radius / width)); // 2
+
+            for (int i = 0; i < lat_moves; i++)
+            {
+                double temp_lat = y + height * i;
+                for (int j = 0; j < lon_moves; j++)
+                {
+                    double temp_lon = x + width * j;
+                    if (IsInsideOfCircle(temp_lat, temp_lon, y, x, radius))
+                    {
+                        var centroid = GetCentroid(temp_lat, temp_lon, height, width);
+                        double x_cen = centroid.x;
+                        double y_cen = centroid.y;
+
+                        points.Add(PointToLatLon(y_cen, x_cen, latitude, longitude));
+                        points.Add(PointToLatLon(-y_cen, x_cen, latitude, longitude));
+                        points.Add(PointToLatLon(y_cen, -x_cen, latitude, longitude));
+                        points.Add(PointToLatLon(-y_cen, -x_cen, latitude, longitude));
+                    }
+                }
+            }
+
+            List<string> geohashes = new List<string>();
+            foreach (Vector2Doubles point in points)
+            {
+                geohashes.Add(GeoHash.Encode(point.x, point.y, precision));
+            }
+            geohashes = geohashes.Distinct().ToList();
+
+            return geohashes.ToArray();
         }
     }
 }
